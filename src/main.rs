241dade42226ba0cae9342ca6c929274c84f5f3c
Pankarinko <pankarinko@gmail.com>
2025-02-17 02:17:38 +0100

    Add user

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,18 @@ use std::{io::{self, Write}, process::{Command, Output}};
 use colored::Colorize;
 
 fn main() {
+  /*Capture output from commands */
   let kernel = Command::new("uname").arg("-r").output();
+  let user = Command::new("whoami").output();
+  /* Output results */
   match str::from_utf8(&kernel.unwrap().stdout) {
-    Ok(version) => println!("{}", version.bright_red()),
+    Ok(version) => { print!("kernel: ");
+        println!("{}", version.bright_red())},
     Err(_) => println!("{}", "No valid output!".red())
+  }
+  match str::from_utf8(&user.unwrap().stdout) {
+    Ok(user) => { print!("user: ");
+    println!("{}", user.bright_blue())},
+    Err(_) => println!("{}", "No valid output!".red()),
   }
   }
